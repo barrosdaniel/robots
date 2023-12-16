3894c6e5ba06e1618af58dce1fdf5b00d1c681cd
Daniel Barros <daniel.barros@cqumail.com>
2023-12-16 13:16:25 +0800

    Complete Setting Up Our Robot.

--- script.py
+++ script.py
@@ -1,1 +1,12 @@
-print('Hello, Daniel!')
+# Define the DriveBot class here!
+class DriveBot:
+    def __init__(self, motor_speed = 0, direction = 0, sensor_range = 0):
+        self.motor_speed = motor_speed
+        self.direction = direction
+        self.sensor_range = sensor_range
+
+robot_1 = DriveBot(5, 90, 10)
+
+print(robot_1.motor_speed)
+print(robot_1.direction)
+print(robot_1.sensor_range)
