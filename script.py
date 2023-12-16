bdefa97d8e2b4255a88c62514aa5b6372c99126c
Daniel Barros <daniel.barros@cqumail.com>
2023-12-16 13:23:00 +0800

    Complete Adding Robot Logic.

--- script.py
+++ script.py
@@ -4,8 +4,17 @@ class DriveBot:
         self.motor_speed = motor_speed
         self.direction = direction
         self.sensor_range = sensor_range
+    
+    def control_bot(self, new_speed, new_direction):
+        self.motor_speed = new_speed
+        self.direction = new_direction
+    
+    def adjust_sensor(self, new_range):
+        self.sensor_range = new_range
 
 robot_1 = DriveBot(5, 90, 10)
+robot_1.control_bot(10, 180)
+robot_1.adjust_sensor(20)
 
 print(robot_1.motor_speed)
 print(robot_1.direction)
