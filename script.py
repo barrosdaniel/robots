4ff9d5b88805d98536293e6e418cd42c3ac38e58
Daniel Barros <daniel.barros@cqumail.com>
2023-12-16 13:49:54 +0800

    Complete Controlling Them All

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
-# Define the DriveBot class here!
 class DriveBot:
+    all_disabled = False
+    latitude = -999999
+    longitude = -999999
+
     def __init__(self, motor_speed = 0, direction = 180, sensor_range = 10):
         self.motor_speed = motor_speed
         self.direction = direction
@@ -12,12 +15,18 @@ class DriveBot:
     def adjust_sensor(self, new_range):
         self.sensor_range = new_range
 
-robot_1 = DriveBot(5, 90, 10)
-robot_1.control_bot(10, 180)
-robot_1.adjust_sensor(20)
+robot_1 = DriveBot()
+robot_1.motor_speed = 5
+robot_1.direction = 90
+robot_1.sensor_range = 10
 
 robot_2 = DriveBot(35, 75, 25)
+robot_3 = DriveBot(20, 60, 10)
 
-print(robot_1.motor_speed)
-print(robot_1.direction)
-print(robot_1.sensor_range)
+DriveBot.latitude = -50
+DriveBot.longitude = 50
+DriveBot.all_disabled = True
+
+print(robot_1.latitude)
+print(robot_2.longitude)
+print(robot_3.all_disabled)
