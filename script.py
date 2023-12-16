a49662b78a9eab81b8a523454e8bd507020f88c2
Daniel Barros <daniel.barros@cqumail.com>
2023-12-16 14:27:43 +0800

    Complete Identifying Robots.

--- script.py
+++ script.py
@@ -2,11 +2,14 @@ class DriveBot:
     all_disabled = False
     latitude = -999999
     longitude = -999999
+    robot_count = 0
 
     def __init__(self, motor_speed = 0, direction = 180, sensor_range = 10):
         self.motor_speed = motor_speed
         self.direction = direction
         self.sensor_range = sensor_range
+        DriveBot.robot_count += 1
+        self.id = DriveBot.robot_count
     
     def control_bot(self, new_speed, new_direction):
         self.motor_speed = new_speed
@@ -23,10 +26,6 @@ robot_1.sensor_range = 10
 robot_2 = DriveBot(35, 75, 25)
 robot_3 = DriveBot(20, 60, 10)
 
-DriveBot.latitude = -50
-DriveBot.longitude = 50
-DriveBot.all_disabled = True
-
-print(robot_1.latitude)
-print(robot_2.longitude)
-print(robot_3.all_disabled)
+print(robot_1.id)
+print(robot_2.id)
+print(robot_3.id)
